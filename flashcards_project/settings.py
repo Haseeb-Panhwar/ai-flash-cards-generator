8f962214f3e776463c39b0a461907ef645a77e1b
My name <me@example.com>
2024-08-17 20:37:28 +0500

    Allowed Hosts edited

--- flashcards_project/settings.py
+++ flashcards_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0no35-$@&39*b=8es3vh)ecjd@2_0!$y(e^&m%kv#en4shi4j)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
